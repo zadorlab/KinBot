38625076b2cee3c6835da1c519c86e61232527ed
Judit Zador <jzador@menten.ca.sandia.gov>
2019-09-24 11:20:52 -0700

    added beta_delta reaction - wrong naming

---
+++ kinbot/reac_beta_gamma.py
@@ -0,0 +1,131 @@
+###################################################
+##                                               ##
+## This file is part of the KinBot code v2.0     ##
+##                                               ##
+## The contents are covered by the terms of the  ##
+## BSD 3-clause license included in the LICENSE  ##
+## file, found at the root.                      ##
+##                                               ##
+## Copyright 2018 National Technology &          ##
+## Engineering Solutions of Sandia, LLC (NTESS). ##
+## Under the terms of Contract DE-NA0003525 with ##
+## NTESS, the U.S. Government retains certain    ##
+## rights to this software.                      ##
+##                                               ##
+## Authors:                                      ##
+##   Judit Zador                                 ##
+##   Ruben Van de Vijver                         ##
+##                                               ##
+###################################################
+import numpy as np
+import copy
+import time
+
+from kinbot import reac_family
+from kinbot import geometry
+
+class betagamma:
+    
+    def __init__(self,species,qc,par,instance,instance_name):
+        #st_pt of the reactant
+        self.species = species
+        #st_pt of the ts
+        self.ts = None
+        #st_pt of the product(s)
+        self.products = []
+        #bond matrix of the products
+        self.product_bonds = [] 
+        
+        #optimization objects
+        self.ts_opt = None
+        self.prod_opt = []
+        
+        self.qc = qc
+        self.par = par
+        
+        #indices of the reactive atoms
+        self.instance = instance
+        #name of the reaction
+        self.instance_name = instance_name
+        
+        #maximum number of steps for this reaction family
+        self.max_step = 12
+        #do a scan?
+        self.scan = 0
+        #skip the first 12 steps in case the instance has a length of 3?
+        self.skip = 0
+
+    def get_constraints(self,step, geom):
+        """
+        There are three types of constraints:
+        1. fix a coordinate to the current value
+        2. change a coordinate and fix is to the new value
+        3. release a coordinate (only for gaussian)
+        """
+        fix = []
+        change = []
+        release = []
+        if step < self.max_step:
+            #fix all the bond lengths
+            for i in range(self.species.natom - 1):
+                for j in range(i+1, self.species.natom):
+                    if self.species.bond[i][j] > 0:
+                        fix.append([i+1,j+1])
+        if step == 0:
+            # verify if the radical atom has more than one neighbor and 
+            # change the dihedral to 90 degrees in that case
+            neigh = [i for i,ni in enumerate(self.species.bond[self.instance[0]]) if ni > 0]
+            if len(neigh) > 1:
+                for ni in neigh:
+                    if not ni in self.instance:
+                        change.append([ni + 1, self.instance[0] + 1, self.instance[1] + 1, self.instance[2] + 1, 90.])
+                        break
+
+        if step < self.max_step:
+            final_dist = 1.42
+            if self.species.atom[self.instance[0]] == 'C' and self.species.atom[self.instance[1]] == 'C' and self.species.atom[self.instance[2]] == 'C':
+                final_dist = 2.20
+            if self.species.atom[self.instance[0]] == 'C' and self.species.atom[self.instance[1]] == 'C' and self.species.atom[self.instance[2]] == 'H':
+                final_dist = 1.79
+            if self.species.atom[self.instance[0]] == 'C' and self.species.atom[self.instance[1]] == 'C' and self.species.atom[self.instance[2]] == 'O':
+                final_dist = 2.04
+            if self.species.atom[self.instance[0]] == 'O' and self.species.atom[self.instance[1]] == 'C' and self.species.atom[self.instance[2]] == 'C':
+                final_dist = 2.12
+            if self.species.atom[self.instance[0]] == 'O' and self.species.atom[self.instance[1]] == 'C' and self.species.atom[self.instance[2]] == 'H':
+                final_dist = 1.84
+            if self.species.atom[self.instance[0]] == 'O' and self.species.atom[self.instance[1]] == 'C' and self.species.atom[self.instance[2]] == 'O':
+                final_dist = 2.04 #TODO: verify if this value is OK
+            if self.species.atom[self.instance[0]] == 'C' and self.species.atom[self.instance[1]] == 'O' and self.species.atom[self.instance[2]] == 'C':
+                final_dist = 2.04 #TODO: verify if this value is OK
+            if self.species.atom[self.instance[0]] == 'C' and self.species.atom[self.instance[1]] == 'O' and self.species.atom[self.instance[2]] == 'H':
+                final_dist = 1.42
+            if self.species.atom[self.instance[0]] == 'C' and self.species.atom[self.instance[1]] == 'O' and self.species.atom[self.instance[2]] == 'O':
+                final_dist = 2.04 #TODO: verify if this value is OK
+            if self.species.atom[self.instance[0]] == 'O' and self.species.atom[self.instance[1]] == 'O' and self.species.atom[self.instance[2]] == 'C':
+                final_dist = 2.04 #TODO: verify if this value is OK
+            
+            val = geometry.new_bond_length(self.species,self.instance[1], self.instance[2], step, self.max_step, final_dist, geom)
+            constraint = [self.instance[1] + 1, self.instance[2] + 1, val]
+            change.append(constraint)
+
+            # breaking the delta bond
+            final_dist = 1.7 # this might need to be refined
+            val = geometry.new_bond_length(self.species,self.instance[3], self.instance[4], step, self.max_step, final_dist, geom)
+            constraint = [self.instance[3] + 1, self.instance[4] + 1, val]
+            change.append(constraint)
+
+        
+        #remove the bonds from the fix if they are in another constaint
+        for c in change:
+            if len(c) == 3:
+                index = -1
+                for i,fi in enumerate(fix):
+                    if len(fi) == 2:
+                        if sorted(fi) == sorted(c[:2]):
+                            index = i
+                if index > -1:
+                    del fix[index]
+        
+        return step, fix, change, release
+
+

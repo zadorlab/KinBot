dfd6707eaa539d2ecd3433e54ebe3aad9fb515f3
Amanda Dewyer <adewyer@menten.ca.sandia.gov>
2019-12-03 12:18:44 -0800

    commented out print lines in reaction_generator.py

--- kinbot/reaction_generator.py
+++ kinbot/reaction_generator.py
@@ -18,7 +18,6 @@
 ##                                               ##
 ###################################################
 from __future__ import print_function
-                        print("start: {}".format(chemid))
 import numpy as np
 import sys
 import os

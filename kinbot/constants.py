addbe8fa93be599fc49f2c8057f5cea2e0ea3002
Amanda Dewyer <adewyer@keck.ca.sandia.gov>
2020-03-10 12:31:46 -0700

    removed block from the top of each file stating the authors etc, block can be found in __init__.py still

--- kinbot/constants.py
+++ kinbot/constants.py
@@ -1,23 +1,3 @@
-###################################################
-##                                               ##
-## This file is part of the KinBot code v2.0     ##
-##                                               ##
-## The contents are covered by the terms of the  ##
-## BSD 3-clause license included in the LICENSE  ##
-## file, found at the root.                      ##
-##                                               ##
-## Copyright 2018 National Technology &          ##
-## Engineering Solutions of Sandia, LLC (NTESS). ##
-## Under the terms of Contract DE-NA0003525 with ##
-## NTESS, the U.S. Government retains certain    ##
-## rights to this software.                      ##
-##                                               ##
-## Authors:                                      ##
-##   Judit Zador                                 ##
-##   Ruben Van de Vijver                         ##
-##                                               ##
-###################################################
-
 AUtoKCAL = 627.5091809
 AUtoCM = 219474.63068
 CMtoKCAL = 0.0028591

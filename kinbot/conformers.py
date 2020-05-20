40a617456fa230f0e5519781d46a72fc8085c4b8
Amanda Dewyer <adewyer@sandia.gov>
2020-05-20 12:10:00 -0700

    updates to number of ring conformers generated
    Update to how we calculate the number of conformers for rings. Instead of 3 ^ (dihed-3) the following would be used:
    - For rings in size 4-6 we leave it as 3 ^ (dihed-3)
    - For ring sizes 7+ we do the following:
        - take the previous number of conformers from the ring structure n-1 (for 7 it would be the # of conformers associated with the 6 member ring) then add 2 ^ (dihed-3) to that value.
    
    The resulting number of conformers would be as follows:
    4-6 member rings = 3 ^ (dihed-3)
        4 mem = 3 ^ (4 - 3) = 3 ^ 1 = 3 confs
        5 mem = 3 ^ (5 - 3) = 3 ^ 2 = 9 confs
        6 mem = 3 ^ (6 - 3) = 3 ^ 3 = 27 confs
    7+ member = (previous # conformers) + (2 ^ (dihed-3))
        7 mem = 27 + 2 ^ 4 = 27 + 16 = 43 confs
        8 mem = 43 + 2 ^ 5 = 43 + 32 = 75 confs
        9 mem = 75 + 2 ^ 6 = 75 + 64 = 138 confs
        ect...

--- kinbot/conformers.py
+++ kinbot/conformers.py
@@ -89,7 +89,20 @@ class Conformers:
                 # number of independent dihedrals
                 nd = len(dihs) - 3
                 # number of conformers for this ring:
-                nc = np.power(3, nd)
+                # 4, 5, 6 member rings nc = 3 ^ nd
+                # 7+ member rings = nc from (ring size - 1) + (2 ^ nd)
+                # ex: 7 member ring = 6 member ring nc + 2 ^ 4 = 27 + 16 = 43
+                if cyc < 7:
+                    nc = np.power(3, nd)
+                else:
+                    baseConf = 27  # 3 ^ 3
+                    nc = baseConf
+                    exp = 4
+                    while exp <= nd:
+                        conf_add = np.power(2, exp)
+                        nc = nc + conf_add
+                        exp = exp + 1
+                             
                 for i in range(nc):
                     self.cyc_dih_atoms.append(random_dihs)
                     # values the dihedrals will be modified to

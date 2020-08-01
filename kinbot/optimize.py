f1b6d6558252150b104b0163e5dc1ea37cdb7379
Judit Zador <jzador@sandia.gov>
2020-08-01 00:39:12 -0700

    fixed merge errors for semi empirical conformer search

--- kinbot/optimize.py
+++ kinbot/optimize.py
@@ -65,7 +65,6 @@ class Optimize:
         self.max_restart = par.par['rotation_restart']
 
     def do_optimization(self):
-        #print("start do_opt {}".format(self.species.chemid))
         while 1:
             # do the conformational search
             if self.par.par['conformer_search'] == 1:
@@ -94,15 +93,75 @@ class Optimize:
                         # ring conf search is finished
                         self.scycconf = 1
                 # do the open chain part of the molecule
-                if self.scycconf == 1:
+             #    if self.scycconf == 1:
 
-                    # do open chain part if cyclic part is done
-                    if self.sconf == -1:
-                        # open chain part has not started yet
+             #        # do open chain part if cyclic part is done
+             #        if self.sconf == -1:
+             #            # open chain part has not started yet
+             #            for geom in self.species.confs.cyc_conf_geoms:
+             #                # take all the geometries from the cyclic part
+             #                # generate the conformers for the current geometry
+             #                self.species.confs.generate_conformers(0, geom)
+             #            # set conf status to running
+             #            self.sconf = 0
+             #        if self.sconf == 0:
+             #            # conformational search is running
+             #            # check if the conformational search is done
+             #            status, lowest_conf, geom, low_energy, conformers, energies = self.species.confs.check_conformers(wait=self.wait)
+             #            if status == 1:
+             #                if self.species.wellorts:
+             #                    self.name = self.species.name
+             #                else:
+             #                    self.name = self.species.chemid
+             #                logging.info("lowest energy conformer for species: {} is number {}".format(self.name, lowest_conf))
+             #                # save lowest energy conformer as species geometry
+             #                self.species.geom = geom
+             #                # save lowest energy conformer energy
+             #                self.species.energy = low_energy
+             #                # set conf status to finished
+             #                self.sconf = 1
+
+                    # first do an semi empirical optimization if requested by the user
+                if self.par.par['semi_emp_conformer_search'] == 1:
+                    if self.ssemi_empconf == -1:
+                        # semi empirical part has not started yet
+                        self.species.semi_emp_confs = Conformers(self.species, self.par, self.qc, semi_emp=1)
                         for geom in self.species.confs.cyc_conf_geoms:
                             # take all the geometries from the cyclic part
                             # generate the conformers for the current geometry
-                            self.species.confs.generate_conformers(0, geom)
+                            self.species.semi_emp_confs.generate_conformers(0, geom)
+                        # set conf status to running
+                        self.ssemi_empconf = 0
+                        if self.ssemi_empconf == 0:
+                            # semi empirical conformational search is running
+                            # check if the conformational search is done
+                            status, lowest_conf, geom, self.semi_emp_low_energy, self.semi_emp_conformers, self.semi_emp_energies = self.species.semi_emp_confs.check_conformers(wait=self.wait)
+                            if status == 1:
+                                if self.species.wellorts:
+                                    self.name = self.species.name
+                                else:
+                                    self.name = self.species.chemid
+                                logging.info("semi empirical lowest energy conformer for species: {} is number {}".format(self.name, lowest_conf))
+                                # set conf status to finished
+                                self.ssemi_empconf = 1
+                else:
+                    self.ssemi_empconf = 1
+                if self.ssemi_empconf == 1:
+                    # do open chain part if cyclic and semi empirical parts are done
+                    if self.sconf == -1:
+                        # open chain part has not started yet
+                        # if semi empirical conformer were searched for, start from those, 
+                        # else start from cyclic conformers
+                        if self.par.par['semi_emp_conformer_search'] == 1:
+                            self.species.confs.nconfs = 1
+                            for i, geom in enumerate(self.semi_emp_conformers):
+                                if (self.semi_emp_energies[i] - self.semi_emp_low_energy) * constants.AUtoKCAL < self.par.par['semi_emp_confomer_threshold']:
+                                    self.species.confs.generate_conformers(0, geom)
+                        else:
+                            for geom in self.species.confs.cyc_conf_geoms:
+                                # take all the geometries from the cyclic part
+                                # generate the conformers for the current geometry
+                                self.species.confs.generate_conformers(0, geom)
                         # set conf status to running
                         self.sconf = 0
                     if self.sconf == 0:
@@ -121,104 +180,6 @@ class Optimize:
                             self.species.energy = low_energy
                             # set conf status to finished
                             self.sconf = 1
-
-                    # first do an semi empirical optimization if requested by the user
-                    if self.par.par['semi_emp_conformer_search'] == 1:
-                        if self.ssemi_empconf == -1:
-                            # semi empirical part has not started yet
-                            self.species.semi_emp_confs = Conformers(self.species, self.par, self.qc, semi_emp=1)
-                            for geom in self.species.confs.cyc_conf_geoms:
-                                # take all the geometries from the cyclic part
-                                # generate the conformers for the current geometry
-                                self.species.semi_emp_confs.generate_conformers(0, geom)
-                            # set conf status to running
-                            self.ssemi_empconf = 0
-                            if self.ssemi_empconf == 0:
-                                # semi empirical conformational search is running
-                                # check if the conformational search is done
-                                status, lowest_conf, geom, self.semi_emp_low_energy, self.semi_emp_conformers, self.semi_emp_energies = self.species.semi_emp_confs.check_conformers(wait=self.wait)
-                                if status == 1:
-                                    if self.species.wellorts:
-                                        self.name = self.species.name
-                                    else:
-                                        self.name = self.species.chemid
-                                    logging.info("semi empirical lowest energy conformer for species: {} is number {}".format(self.name, lowest_conf))
-                                    # set conf status to finished
-                                    self.ssemi_empconf = 1
-                    else:
-                        self.ssemi_empconf = -1
-                    if self.ssemi_empconf == 1:
-                        # do open chain part if cyclic and semi empirical parts are done
-                        if self.sconf == -1:
-                            # open chain part has not started yet
-                            # if semi empirical conformer were searched for, start from those, 
-                            # else start from cyclic conformers
-                            if self.par.par['semi_emp_conformer_search'] == 1:
-                                self.species.confs.nconfs = 1
-                                for i, geom in enumerate(self.semi_emp_conformers):
-                                    if (self.semi_emp_energies[i] - self.semi_emp_low_energy) * constants.AUtoKCAL < self.par.par['semi_emp_confomer_threshold']:
-                                        self.species.confs.generate_conformers(0, geom)
-                            else:
-                                self.ssemi_empconf = -1  # TODO not sure about this line
-                    # first do an semi empirical optimization if requested by the user
-                    if self.par.par['semi_emp_conformer_search'] == 1:
-                        if self.ssemi_empconf == -1:
-                            # semi empirical part has not started yet
-                            self.species.semi_emp_confs = Conformers(self.species, self.par, self.qc, semi_emp=1)
-                            for geom in self.species.confs.cyc_conf_geoms:
-                                # take all the geometries from the cyclic part
-                                # generate the conformers for the current geometry
-                                self.species.semi_emp_confs.generate_conformers(0, geom)
-                            # set conf status to running
-                            self.ssemi_empconf = 0
-                            if self.ssemi_empconf == 0:
-                                # semi empirical conformational search is running
-                                # check if the conformational search is done
-                                status, lowest_conf, geom, self.semi_emp_low_energy, self.semi_emp_conformers, self.semi_emp_energies = self.species.semi_emp_confs.check_conformers(wait=self.wait)
-                                if status == 1:
-                                    if self.species.wellorts:
-                                        self.name = self.species.name
-                                    else:
-                                        self.name = self.species.chemid
-                                    logging.info("semi empirical lowest energy conformer for species: {} is number {}".format(self.name, lowest_conf))
-                                    # set conf status to finished
-                                    self.ssemi_empconf = 1
-                    else:
-                        self.ssemi_empconf = -1
-                    if self.ssemi_empconf == 1:
-                        # do open chain part if cyclic and semi empirical parts are done
-                        if self.sconf == -1:
-                            # open chain part has not started yet
-                            # if semi empirical conformer were searched for, start from those, 
-                            # else start from cyclic conformers
-                            if self.par.par['semi_emp_conformer_search'] == 1:
-                                self.species.confs.nconfs = 1
-                                for i, geom in enumerate(self.semi_emp_conformers):
-                                    if (self.semi_emp_energies[i] - self.semi_emp_low_energy) * constants.AUtoKCAL < self.par.par['semi_emp_confomer_threshold']:
-                                        self.species.confs.generate_conformers(0, geom)
-                            else:
-                                for geom in self.species.confs.cyc_conf_geoms:
-                                    # take all the geometries from the cyclic part
-                                    # generate the conformers for the current geometry
-                                    self.species.confs.generate_conformers(0, geom)
-                            # set conf status to running
-                            self.sconf = 0
-                        if self.sconf == 0:
-                            # conformational search is running
-                            # check if the conformational search is done
-                            status, lowest_conf, geom, low_energy, conformers, energies = self.species.confs.check_conformers(wait=self.wait)
-                            if status == 1:
-                                if self.species.wellorts:
-                                    self.name = self.species.name
-                                else:
-                                    self.name = self.species.chemid
-                                logging.info("lowest energy conformer for species: {} is number {}".format(self.name, lowest_conf))
-                                # save lowest energy conformer as species geometry
-                                self.species.geom = geom
-                                # save lowest energy conformer energy
-                                self.species.energy = low_energy
-                                # set conf status to finished
-                                self.sconf = 1
             else:
                 # no conf search necessary, set status to finished
                 self.sconf = 1

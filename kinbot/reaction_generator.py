09f5c9312f83636d4f1ca78e98262c71cca3bf72
Judit Zador <jzador@sandia.gov>
2020-09-15 09:17:36 -0700

    run all the way, still bug in bimolecular mess file name

--- kinbot/reaction_generator.py
+++ kinbot/reaction_generator.py
@@ -410,7 +410,7 @@ class ReactionGenerator:
                             if not str(chemid) in jobs:
                                 # this well is new, add it to the jobs
                                 while 1:
-                                    #logging.info('{} {} {} {}'.format(self.par['input_file'], obj.products[0], new_barrier_threshold, dirwell))
+                                    logging.info('lalala {} {} {} {}'.format(self.par['input_file'], obj.products[0], new_barrier_threshold, dirwell))
                                     try:
                                         # try to open the file and write to it
                                         pes.write_input(self.inp, obj.products[0], new_barrier_threshold, dirwell)

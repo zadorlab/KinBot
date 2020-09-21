88f9be68449c3a07f45ec5e4d21c04e96942ed10
Judit Zador <jzador@sandia.gov>
2020-09-21 16:18:33 -0700

    changed the uq code

--- kinbot/uncertaintyAnalysis.py
+++ kinbot/uncertaintyAnalysis.py
@@ -16,39 +16,51 @@ class UQ:
         self.barUQ = par['barrier_uq']
         self.freqUQ = par['freq_uq']
         self.imagFreqUQ = par['imagfreq_uq']
+        self.uq_iter = 0
+
 
     def calc_factor(self, propertyType, species, uq_iter):
+        if self.uq_iter != uq_iter:  # new iteration
+            with open('uqtk.dat', 'a') as f:
+                f.write('')  # new line
+
         if uq_iter == 0:
             if propertyType == 'freq' or propertyType == 'imagfreq':
                 factor = 1
             else:
                 factor = 0
-        
-            self.write_file(propertyType, factor, species, uq_iter)
-
+            self.write_uqtk_header(species, propertyType)
             return factor
         
         if propertyType == 'energy':
             factor = random.uniform(-self.wellUQ, self.wellUQ)
+            normfactor = factor / self.wellUQ
 
         elif propertyType == 'freq':
-            factorMin = 1.0 / self.freqUQ
-            factor = random.uniform(factorMin, self.freqUQ)
+            # Amanda, this is the right way to sample from a log normal distribution
+            factor = np.exp(random.uniform(np.log(1./self.freqUQ), np.log(self.freqUQ)))
+            normfactor = np.log(factor) / np.log(self.freqUQ)
 
         elif propertyType == 'imagFreq':
-            factorMin = 1.0 / self.imagFreqUQ
-            factor = random.uniform(factorMin, self.imagFreqUQ)
+            factor = np.exp(random.uniform(np.log(1./self.imagfreqUQ), np.log(self.imagfreqUQ)))
+            normfactor = np.log(factor) / np.log(self.imagfreqUQ)
 
         elif propertyType == 'barrier':
             factor = random.uniform(-self.barUQ, self.barUQ)
+            normfactor = factor / self.barUQ
 
-        self.write_file(propertyType, factor, species, uq_iter)
+        self.write_uqtk_data(normfactor)
 
         return factor
 
-    def write_file(self, propertyType, propertyValue, species, uq_iter):
-        uqfile = 'uq_' + propertyType + '.txt'
-        with open(uqfile, 'a') as fi:
-            fi.write("{}, {}, {}\n".format(uq_iter, species, propertyValue))
-        fi.close()
+
+    def write_uqtk_header(self, species, propertyType):
+        with open('uqtk.dat', 'a') as fi:
+            fi.write("{}/{}".format(species, propertyType.rstrip('\n')))
+        return 0
+
+
+    def write_uqtk_data(self, normfactor)
+        with open('uqtk.dat', 'a') as fi:
+            fi.write("{}".format(normfactor.rstrip('\n')))
         return 0

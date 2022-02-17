70ade20c77a3894e3639ee02cc4c2b465b765d8c
Judit Zador <jzador@sandia.gov>
2022-02-17 11:21:48 -0800

    fixed irc tpl≈

--- tpl/ase_gauss_irc.tpl.py
+++ tpl/ase_gauss_irc.tpl.py
@@ -16,6 +16,8 @@ Gaussian.command = '{qc_command} < PREFIX.com > PREFIX.log'
 calc = Gaussian(**kwargs)
 mol.set_calculator(calc)
 
+success = True
+
 try:
     e = mol.get_potential_energy() # use the Gaussian optimizer
     #Positions (geom) updated in ase/ases/io/gaussian.py code    
@@ -26,24 +28,26 @@ except:
         db.write(mol, name='{label}', data={{'status': 'normal'}}) #although there is an error, continue from the final geometry
     else:
         db.write(mol, name='{label}', data={{'status': 'error'}})
-        return -1
-
-# start the product optimization
-prod_kwargs = {prod_kwargs}
-calc_prod = Gaussian(**prod_kwargs)
-mol_prod = Atoms(symbols={atom}, positions=mol.positions)
-mol_prod.set_calculator(calc_prod)
-try:
-    e = mol_prod.get_potential_energy() # use the Gaussian optimizer
-    db.write(mol, name='{label}_prod', data={{'energy': e,'status': 'normal'}})
-except RuntimeError: 
-    mol_prod.positions = reader_gauss.read_geom('{label}_prod.log', mol_prod, dummy)
-    if mol_prod.positions is not None:
-        db.write(mol_prod, name='{label}_prod', data={{'status': 'normal'}}) 
-    else:
-        db.write(mol_prod, name='{label}_prod', data={{'status': 'error'}})
+        success = False
 
 with open('{label}.log', 'a') as f:
     f.write('done\n')
-with open('{label}_prod.log', 'a') as f:
-    f.write('done\n')
+
+if success:
+    # start the product optimization
+    prod_kwargs = {prod_kwargs}
+    calc_prod = Gaussian(**prod_kwargs)
+    mol_prod = Atoms(symbols={atom}, positions=mol.positions)
+    mol_prod.set_calculator(calc_prod)
+    try:
+        e = mol_prod.get_potential_energy() # use the Gaussian optimizer
+        db.write(mol, name='{label}_prod', data={{'energy': e,'status': 'normal'}})
+    except RuntimeError: 
+        mol_prod.positions = reader_gauss.read_geom('{label}_prod.log', mol_prod, dummy)
+        if mol_prod.positions is not None:
+            db.write(mol_prod, name='{label}_prod', data={{'status': 'normal'}}) 
+        else:
+            db.write(mol_prod, name='{label}_prod', data={{'status': 'error'}})
+
+    with open('{label}_prod.log', 'a') as f:
+        f.write('done\n')

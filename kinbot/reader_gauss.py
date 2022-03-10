e3404ba18f6c51d36a7282b1a6260b8a4dfe5c94
Judit Zador <jzador@sandia.gov>
2022-03-09 16:44:58 -0800

    updated ring conformer template, using constraints fn and changed to degrees units

--- kinbot/reader_gauss.py
+++ kinbot/reader_gauss.py
@@ -16,7 +16,7 @@ def read_energy(outfile):
     with open(outfile) as f:
         lines = f.readlines()
 
-    energy = 0.
+    energy = np.NAN
     for line in reversed(lines):
         if 'SFC Done' in line:
             energy = float(line.split()[4]) / constants.EVtoHARTREE
@@ -50,7 +50,7 @@ def read_zpe(outfile):
     with open(outfile) as f:
         lines = f.readlines()
 
-    zpe = 100.
+    zpe = np.NAN
     for line in reversed(lines):
         if 'Zero-point correction=' in line:
             zpe = float(line.split()[2])
@@ -110,7 +110,9 @@ def read_convergence(outfile):
 
 def constraint(mol, fix, change):
     """
-    Convert constraints into PCBFGS constraints.
+    Convert constraints into ASE constraints.
+    Angles are in degrees according to angles_deg and dihedrals_deg.
+    Careful: atom indices in the fix and change lists start at 1
     """
 
     bonds = []
@@ -118,29 +120,23 @@ def constraint(mol, fix, change):
     dihedrals = []
     for fi in fix:
         if len(fi) == 2:
-            #careful: atom indices in the fix lists start at 1
             bondlength = mol.get_distance(fi[0]-1, fi[1]-1)
             bonds.append([bondlength,[fi[0]-1, fi[1]-1]])
         if len(fi) == 3:
-            #careful: atom indices in the fix lists start at 1
-            angle = mol.get_angle(fi[0]-1, fi[1]-1, fi[2]-1) * np.pi / 180.
+            angle = mol.get_angle(fi[0]-1, fi[1]-1, fi[2]-1)
             angles.append([angle,[fi[0]-1, fi[1]-1, fi[2]-1]])
         if len(fi) == 4:
-            #careful: atom indices in the fix lists start at 1
-            dihed = mol.get_dihedral(fi[0]-1, fi[1]-1, fi[2]-1, fi[3]-1) * np.pi / 180.
+            dihed = mol.get_dihedral(fi[0]-1, fi[1]-1, fi[2]-1, fi[3]-1)
             dihedrals.append([dihed,[fi[0]-1, fi[1]-1, fi[2]-1, fi[3]-1]])
     for ci in change:
         if len(ci) == 3:
-            #careful: atom indices in the fix lists start at 1
             bondlength = ci[2]
             bonds.append([bondlength,[ci[0]-1, ci[1]-1]])
         if len(ci) == 4:
-            #careful: atom indices in the fix lists start at 1
-            angle = ci[3] * np.pi / 180.
+            angle = ci[3]
             angles.append([angle, [ci[0]-1, ci[1]-1, ci[2]-1]])
         if len(ci) == 5:
-            #careful: atom indices in the fix lists start at 1
-            dihed = ci[4] * np.pi / 180.
+            dihed = ci[4]
             dihedrals.append([dihed,[ci[0]-1, ci[1]-1, ci[2]-1, ci[3]-1]])
 
     return bonds, angles, dihedrals
--- tpl/ase_gauss_ring_conf.tpl.py
+++ tpl/ase_gauss_ring_conf.tpl.py
@@ -5,6 +5,7 @@ from ase.calculators.gaussian import Gaussian
 from ase.optimize import LBFGS
 from ase.constraints import FixInternals
 from ase.db import connect
+from kinbot import reader_gauss
 
 db = connect('{working_dir}/kinbot.db')
 mol = Atoms(symbols={atom}, positions={geom})
@@ -14,45 +15,11 @@ Gaussian.command = '{qc_command} < PREFIX.com > PREFIX.log'
 calc = Gaussian(**kwargs)
 mol.calc = calc
 
-fix = {fix}
-change = {change}
-
-bonds = []
-angles = []
-dihedrals = []
-
-for fi in fix:
-    if len(fi) == 2:
-        #careful: atom indices in the fix lists start at 1
-        bondlength = mol.get_distance(fi[0]-1, fi[1]-1)
-        bonds.append([bondlength,[fi[0]-1, fi[1]-1]])
-    if len(fi) == 3:
-        #careful: atom indices in the fix lists start at 1
-        angle = mol.get_angle(fi[0]-1, fi[1]-1, fi[2]-1) * np.pi / 180
-        angles.append([angle,[fi[0]-1, fi[1]-1, fi[2]-1]])
-    if len(fi) == 4:
-        #careful: atom indices in the fix lists start at 1
-        dihed = mol.get_dihedral(fi[0]-1, fi[1]-1, fi[2]-1, fi[3]-1) * np.pi / 180
-        dihedrals.append([dihed,[fi[0]-1, fi[1]-1, fi[2]-1, fi[3]-1]])
-for ci in change:
-    if len(ci) == 3:
-        #careful: atom indices in the fix lists start at 1
-        bondlength = ci[2]
-        bonds.append([bondlength,[ci[0]-1, ci[1]-1]])
-    if len(ci) == 4:
-        #careful: atom indices in the fix lists start at 1
-        angle = ci[3] * np.pi / 180
-        angles.append([angle,[ci[0]-1, ci[1]-1, ci[2]-1]])
-    if len(ci) == 5:
-        #careful: atom indices in the fix lists start at 1
-        dihed = ci[4] * np.pi / 180
-        dihedrals.append([dihed,[ci[0]-1, ci[1]-1, ci[2]-1, ci[3]-1]])
-
-constraints = FixInternals(bonds=bonds, angles=angles, dihedrals=dihedrals)
+bonds, angles, dihedrals = reader_gauss.constraint(mol, {fix}, {change})
+constraints = FixInternals(bonds=bonds, angles_deg=angles, dihedrals_deg=dihedrals)
 mol.set_constraint(constraints)
 
 dyn = LBFGS(atoms=mol, trajectory='ringopt.traj')
-
 try:
     dyn.run(fmax=0.01, steps=400)
     e = mol.get_potential_energy()

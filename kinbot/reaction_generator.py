538df78a6cc9c941f42f84368634adff92b28a9f
Judit Zador <jzador@sandia.gov>
2022-04-25 15:44:29 -0700

    added me parameter in reactiong_generator to pes.write_input

--- kinbot/reaction_generator.py
+++ kinbot/reaction_generator.py
@@ -484,7 +484,7 @@ class ReactionGenerator:
                                 while 1:
                                     try:
                                         # try to open the file and write to it
-                                        pes.write_input(self.inp, obj.products[0], new_barrier_threshold, dirwell)
+                                        pes.write_input(self.inp, obj.products[0], new_barrier_threshold, dirwell, self.par['me'])
                                         with open(dirwell + '/chemids', 'a') as f:
                                             f.write('{}\n'.format(chemid))
                                         break

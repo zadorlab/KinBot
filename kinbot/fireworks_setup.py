72aa132dc52103bcf8351e1404e3990cd5017a50
Carles MartÃ <cmartia@sandia.gov>
2022-04-26 17:44:29 -0700

    Cleaned code and improed docstring.

--- kinbot/fireworks_setup.py
+++ kinbot/fireworks_setup.py
@@ -1,8 +1,18 @@
+"""Module to manage the execution of FireWorks.
+
+"""
 import os
 import logging
 
 
 def reset_lpad(lpad):
+    """Eliminates the jobs of former kinbot executions from the launchapd.
+
+    @param lpad: LaunchPad object to manage Workflows, FireWorks and tasks.
+
+    This is mainly used to prevent fireworks tasks to remain in the RUN status
+    after a job reaches its walltime.
+    """
     import json
     pids = []
     if os.path.isfile('pids'):
@@ -12,6 +22,7 @@ def reset_lpad(lpad):
         with open('FW.json', 'r') as json_fh:
             fw_dict = json.load(json_fh)
             pids = [fw_dict['name']]
+
     for fw_id in lpad.get_fw_ids():
         fw = lpad.get_fw_by_id(fw_id)
         if fw.name in pids:
@@ -19,8 +30,11 @@ def reset_lpad(lpad):
 
 
 def load_launchpad(lpad_file, **kwargs):  # TODO build a LaunchPad from kinbot parameters
-    """Load a launchpad from a .yaml file and remove all its workflows."""
-    from datetime import date
+    """Load a launchpad from a .yaml file and remove reset the present fireworks.
+
+    @param lpad_file: Path to the yaml file containing the settings for Fireworks
+        to connect to the MongoDB FireServer storing all the tasks to carry out.
+    """
     from fireworks import LaunchPad
 
     if os.path.isfile(lpad_file):
@@ -61,11 +75,21 @@ def load_launchpad(lpad_file, **kwargs):  # TODO build a LaunchPad from kinbot p
 
 
 def load_qadapter(qadapter_file, **kwargss):  # TODO build a QueueAdapter from kinbot parameters
+    """Load a QAdapter from a yaml file or build one from KinBot input parameters.
+
+    @param qadapter_file: Path to the yaml file containing the settings for Fireworks
+        to manage the scheduler operations.
+    """
     from fireworks.queue.queue_adapter import QueueAdapterBase
     return QueueAdapterBase.from_file(qadapter_file)
 
 
 def load_fworker(fworker_file, **kwargs):  # TODO build a FWorker from kinbot parameters
+    """Load a FWorker from a yaml file or build one from KinBot input parameters
+
+    @param fworker_file: Path to the yaml file containing the settings for Fireworks
+        to manage the worker carrying out the calculations.
+    """
     from fireworks import FWorker
     if os.path.isfile(fworker_file):
         pass
@@ -77,13 +101,20 @@ def load_fworker(fworker_file, **kwargs):  # TODO build a FWorker from kinbot pa
         return FWorker()
 
 
-def place_job(lpad, cmd, name="KinBot workflow"):
+def place_job(lpad, cmd, wf_name="KinBot workflow"):
+    """Place a jobn in the LaunchPad server for a FireWorker to execute.
+
+    @param lpad: LaunchPad object to manage Workflows, FireWorks and tasks.
+    @param cmd: Command to execute in the FireWorks task.
+    @param wf_name: Name of the Task to be placed.
+
+    """
     from fireworks import Workflow, Firework, ScriptTask
     firetask = ScriptTask.from_str(cmd)
     firework = Firework(firetask)
     firework.spec['_launch_dir'] = os.getcwd()
     firework.name = os.getpid()
-    workflow = Workflow([firework], name=name)
+    workflow = Workflow([firework], name=wf_name)
     lpad.add_wf(workflow)
 
 
@@ -91,6 +122,9 @@ def setup_fireworks(lpad_file='my_launchpad.yaml',
                     fworker_file='my_fworker.yaml',
                     qadapter_file='my_qadapter.yaml',
                     **kwargs):
+    """
+
+    """
     from subprocess import Popen
     import time
 
@@ -108,7 +142,6 @@ def setup_fireworks(lpad_file='my_launchpad.yaml',
     # TODO use python API
     cmd = f"qlaunch -fm singleshot"
     # cmd = f"qlaunch --launch_dir {os.getcwd()} --block_dir block_fake -fm rapidfire -m 3 --nlaunches infinite"
-
     for i in range(num_jobs):
         Popen(cmd, shell=True)
         time.sleep(1)

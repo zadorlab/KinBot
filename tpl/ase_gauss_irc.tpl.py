5ec47c5950ef43ee73173e7b291310dfb4603c0a
Carles Martí <cmartia@sandia.gov>
2022-08-26 09:58:00 -0700

    Corrected bug: Calculations were not written in the database when the failed in the first attempt but they succeed in the second. Corrected minor stylistic issues.

--- tpl/ase_gauss_irc.tpl.py
+++ tpl/ase_gauss_irc.tpl.py
@@ -18,7 +18,7 @@ mol.calc = calc
 success = True
 
 try:
-    e = mol.get_potential_energy() # use the Gaussian optimizer
+    e = mol.get_potential_energy()  # use the Gaussian optimizer
     iowait(logfile, 'gauss')
     mol.positions = reader_gauss.read_geom(logfile, mol)
     db.write(mol, name=label, data={{'energy': e, 'status': 'normal'}})
@@ -30,6 +30,7 @@ except RuntimeError:
         kwargs = reader_gauss.correct_kwargs(logfile, kwargs)
         mol.calc = Gaussian(**kwargs)
         e = mol.get_potential_energy()  # use the Gaussian optimizer
+        db.write(mol, name=label, data={{'energy': e, 'status': 'normal'}})
     except RuntimeError:
         if mol.positions is not None:
             # although there is an error, continue from the final geometry

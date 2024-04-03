2aad0531058b9116498b1d9ad6d377f676da4de4
csoulie <csoulie@sandia.gov>
2024-04-03 13:27:50 -0700

    vdW bug with freq

--- kinbot/optimize.py
+++ kinbot/optimize.py
@@ -351,7 +351,9 @@ class Optimize:
                 symmetry.calculate_symmetry(self.species)
 
                 # calculate the new frequencies with the internal rotations projected out
-                if self.par['multi_conf_tst'] == 0 and self.par['rotor_scan'] and not self.VTS:
+                if self.par['multi_conf_tst'] == 0 and self.par['rotor_scan']\
+                    and not self.VTS\
+                    and not self.just_high:
                     fr_file = self.log_name(self.par['high_level'])
                     hess = self.qc.read_qc_hess(fr_file, self.species.natom)
                     if self.qc.qc == 'qchem':

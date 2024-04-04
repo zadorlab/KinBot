1750ee28f21a307076471b1f0187ca7ba1f7ae97
Carles Martí <cmartia@sandia.gov>
2024-04-03 19:19:45 -0700

    Fix bug with hindered rotors.

--- kinbot/optimize.py
+++ kinbot/optimize.py
@@ -297,6 +297,8 @@ class Optimize:
                                             self.shir = 1
                                     else:
                                         self.shir = 1
+                                else:
+                                    self.shir = 1
                         else:
                             # no hir calculations necessary, set status to finished
                             self.shir = 1

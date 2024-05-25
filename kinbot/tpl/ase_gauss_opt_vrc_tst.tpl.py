227587962dbe41222b680fd8c3fa02ac198111e3
Judit Zador <jzador@sandia.gov>
2024-05-24 18:45:26 -0700

    typo fixed

--- kinbot/tpl/ase_gauss_opt_vrc_tst.tpl.py
+++ kinbot/tpl/ase_gauss_opt_vrc_tst.tpl.py
@@ -185,7 +185,7 @@ except:
                 if 'frozen' in label:
                     kwargs.pop('opt', None)
                 else:
-                    kwargs['addsec'] = kwargs['addsec'].split('\n')[0]  # keep the part for the fixed bond
+                    kwargs['addsec'] = kwargs['addsec'].split('\n')[0] + '\n'  # keep the part for the fixed bond
                     for param in initial_inter_frag:
                         for indx in param[:-1]:
                             kwargs['addsec'] += f'{{indx+1}} '

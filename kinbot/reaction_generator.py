6abebd30e27b890f89b9c362c3af07bcd7506967
Judit Zador <jzador@sandia.gov>
2025-01-04 17:18:40 -0800

    correcting typo

--- kinbot/reaction_generator.py
+++ kinbot/reaction_generator.py
@@ -389,16 +389,16 @@ class ReactionGenerator:
                             logger.info('\tChecking vdW well for {}.'.format(obj.instance_name))
                             obj.irc_prod.characterize()
                             try:
-                                e, obj.irc_prod.energy = self.qc.get_qc_energy(f'{obj.irc_prod.name}') #e is the error code: should be 0 (success) at this point.
+                                e, obj.irc_prod.energy = self.qc.get_qc_energy(f'{obj.irc_prod.name}')  # e is the error code: should be 0 (success) at this point.
                                 e, obj.irc_prod.zpe = self.qc.get_qc_zpe(f'{obj.irc_prod.name}')
                                 e, obj.irc_prod.geom = self.qc.get_qc_geom(obj.irc_prod.name, obj.irc_prod.natom)
                                 e, obj.irc_prod.freq = self.qc.get_qc_freq(obj.irc_prod.name, obj.irc_prod.natom) 
                                 for this_frag in obj.irc_fragments:
                                     e, this_frag.freq = self.qc.get_qc_freq(f'{this_frag.name}_well', this_frag.natom) 
                                 fragments_energies = sum([(this_frag.energy + this_frag.zpe) for this_frag in obj.irc_fragments ])
-                                obj.vdW_depth = (fragments_energies - (obj.irc_prod.energy + obj.irc_prod.zpe))*constants.AUtoKCAL
+                                obj.vdW_depth = (fragments_energies - (obj.irc_prod.energy + obj.irc_prod.zpe)) * constants.AUtoKCAL
                                 if obj.vdW_depth > self.par['vdW_detection']:
-                                    logger.info(f'\tvdW well detected for {irc_prod.nam}. Depth: {roun(obj.vdW_depth, 2)} kcal/mol.')
+                                    logger.info(f'\tvdW well detected for {irc_prod.nam}. Depth: {round(obj.vdW_depth, 2)} kcal/mol.')
                                     obj.do_vdW = True
                             except:
                                 logger.info('\t{} was not succesfull, vdW search stopped for this well.'.format(obj.irc_prod.name))
